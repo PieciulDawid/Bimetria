f9993ea242bf0f0d3f2abb378539f44242ca89c7
dawiddmitruk <adres.do.korespondencji@o2.pl>
2022-03-23 04:26:14 +0100

    obliczanie progu dla metod Kapur i LuWu w ThresholdingUtil.java

--- PB/src/main/java/com/example/zadanie1/ThresholdingUtil.java
+++ PB/src/main/java/com/example/zadanie1/ThresholdingUtil.java
@@ -1,29 +1,107 @@
 package com.example.zadanie1;
 
-import java.awt.image.BufferedImage;
+import java.util.function.BiFunction;
+import java.util.function.ToDoubleBiFunction;
 
 public class ThresholdingUtil {
 	
 	public static int findThresholdForOtsus(int[] histogramData, int pixelCount) {
+		BiFunction<int[], Integer, ToDoubleBiFunction<Integer, Integer>> a =
+				(int[] _histogramData, Integer _pixelCount) -> {
+			
+			return (Integer lessThenThresholdCount, Integer currThreshold) -> {
+				double omega0 = lessThenThresholdCount / (double) _pixelCount;
+				
+				double stDev0 = standardDev(
+						_histogramData,
+						0,
+						currThreshold,
+						lessThenThresholdCount);
+				double stDev1 = standardDev(
+						_histogramData,
+						currThreshold,
+						_histogramData.length,
+						_pixelCount - lessThenThresholdCount);
+				
+				
+				return omega0 * stDev0 + (1 - omega0) * stDev1;
+			};
+		};
+		
+		return findThresholdWithHighestMeasure(histogramData, pixelCount, a);
+	}
+	
+	public static int findThresholdForKapurs(int[] histogramData, int pixelCount) {
+		BiFunction<int[], Integer, ToDoubleBiFunction<Integer, Integer>> a =
+				(int[] _histogramData, Integer _pixelCount) -> {
+					
+					return (Integer lessThenThresholdCount, Integer currThreshold) -> {
+						double entropy0 = entropy(
+								_histogramData,
+								0,
+								currThreshold,
+								lessThenThresholdCount);
+						
+						double entropy1 = entropy(
+								_histogramData,
+								currThreshold,
+								_histogramData.length,
+								_pixelCount - lessThenThresholdCount);
+						
+						
+						return entropy0 + entropy1;
+					};
+				};
+		
+		return findThresholdWithHighestMeasure(histogramData, pixelCount, a);
+	}
+	
+	public static int findThresholdForLuWu(int[] histogramData, int pixelCount) {
+		BiFunction<int[], Integer, ToDoubleBiFunction<Integer, Integer>> a =
+				(int[] _histogramData, Integer _pixelCount) -> {
+					
+					return (Integer lessThenThresholdCount, Integer currThreshold) -> {
+						double entropy0 = entropy(
+								_histogramData,
+								0,
+								currThreshold,
+								lessThenThresholdCount);
+						
+						double entropy1 = entropy(
+								_histogramData,
+								currThreshold,
+								_histogramData.length,
+								_pixelCount - lessThenThresholdCount);
+						
+						
+						return -(entropy1 - entropy0);
+					};
+				};
+		
+		return findThresholdWithHighestMeasure(histogramData, pixelCount, a);
+	}
+	
+	
+	public static int findThresholdWithHighestMeasure(
+			int[] histogramData,
+			int pixelCount,
+			BiFunction<int[], Integer, ToDoubleBiFunction<Integer, Integer>> measureCalculatorSupplier) {
+		
 		double valueOfMax = 0;
 		int indexOfMax = 0;
 		
 		int lessThenThresholdCount = 0;
 		
+		var measureCalculator =
+				measureCalculatorSupplier.apply(histogramData, pixelCount);
+		
 		for (int currThreshold = 1; currThreshold < histogramData.length; currThreshold++) {
 			lessThenThresholdCount += histogramData[currThreshold];
 			
-			double currVariance = 0;
-			double omega0 = lessThenThresholdCount / (double) pixelCount;
+			double currMeasureValue = measureCalculator.applyAsDouble(lessThenThresholdCount, currThreshold);
 			
-			double stDev0 = standardDev(histogramData, 0, currThreshold, lessThenThresholdCount);
-			double stDev1 = standardDev(histogramData, currThreshold, 256, pixelCount - lessThenThresholdCount);
-			
-			
-			currVariance = omega0 * stDev0 + (1 - omega0) * stDev1;
-			
-			if (valueOfMax < currVariance) {
-				valueOfMax = currVariance;
+			if (valueOfMax < currMeasureValue) {
+				valueOfMax = currMeasureValue;
 				indexOfMax = currThreshold;
 			}
 		}
@@ -51,5 +129,17 @@ public class ThresholdingUtil {
 		}
 		
 		return sum / count;
+	}
+	
+	private static double entropy(int[] histogramData, int start, int end, int count) {
+		double sum = 0;
+		
+		for (int i = start; i < end; i++) {
+			final double conditionalP = histogramData[i] / (double) count;
+			
+			sum += conditionalP * Math.log(conditionalP);
+		}
+		
+		return -sum;
 	}
 }
