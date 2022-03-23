d04671e6d467d4d276b65953bc4c37349d73fc6e
dawiddmitruk <adres.do.korespondencji@o2.pl>
2022-03-23 16:12:36 +0100

    Refactor Kapura i LuWu

--- PB/src/main/java/com/example/zadanie1/ThresholdingUtil.java
+++ PB/src/main/java/com/example/zadanie1/ThresholdingUtil.java
@@ -1,104 +1,102 @@
 package com.example.zadanie1;
 
-import java.util.function.BiFunction;
-import java.util.function.ToDoubleBiFunction;
-
 public class ThresholdingUtil {
 	
 	public static int findThresholdForOtsus(int[] histogramData, int pixelCount) {
-		BiFunction<int[], Integer, ToDoubleBiFunction<Integer, Integer>> a =
-				(int[] _histogramData, Integer _pixelCount) -> {
+		MeasureCalculator measureCalculator = (
+				_histogramData,
+				_pixelCount,
+				lessThenThresholdCount,
+				currThreshold) -> {
 			
-			return (Integer lessThenThresholdCount, Integer currThreshold) -> {
-				double omega0 = lessThenThresholdCount / (double) _pixelCount;
-				
-				double stDev0 = standardDev(
-						_histogramData,
-						0,
-						currThreshold,
-						lessThenThresholdCount);
-				double stDev1 = standardDev(
-						_histogramData,
-						currThreshold,
-						_histogramData.length,
-						_pixelCount - lessThenThresholdCount);
-				
-				
-				return omega0 * stDev0 + (1 - omega0) * stDev1;
-			};
+			double omega0 = lessThenThresholdCount / (double) _pixelCount;
+			
+			double stDev0 = standardDev(
+					_histogramData,
+					0,
+					currThreshold,
+					lessThenThresholdCount);
+			double stDev1 = standardDev(
+					_histogramData,
+					currThreshold,
+					_histogramData.length,
+					_pixelCount - lessThenThresholdCount);
+			
+			
+			return omega0 * stDev0 + (1 - omega0) * stDev1;
 		};
 		
-		return findThresholdWithHighestMeasure(histogramData, pixelCount, a);
+		return findThresholdWithHighestMeasure(histogramData, pixelCount, measureCalculator);
 	}
 	
 	public static int findThresholdForKapurs(int[] histogramData, int pixelCount) {
-		BiFunction<int[], Integer, ToDoubleBiFunction<Integer, Integer>> a =
-				(int[] _histogramData, Integer _pixelCount) -> {
-					
-					return (Integer lessThenThresholdCount, Integer currThreshold) -> {
-						double entropy0 = entropy(
-								_histogramData,
-								0,
-								currThreshold,
-								lessThenThresholdCount);
-						
-						double entropy1 = entropy(
-								_histogramData,
-								currThreshold,
-								_histogramData.length,
-								_pixelCount - lessThenThresholdCount);
-						
-						
-						return entropy0 + entropy1;
-					};
-				};
+		MeasureCalculator measureCalculator = (
+				_histogramData,
+				_pixelCount,
+				lessThenThresholdCount,
+				currThreshold) -> {
+			
+			double entropy0 = entropy(
+					_histogramData,
+					0,
+					currThreshold,
+					lessThenThresholdCount);
+			
+			double entropy1 = entropy(
+					_histogramData,
+					currThreshold,
+					_histogramData.length,
+					_pixelCount - lessThenThresholdCount);
+			
+			
+			return entropy0 + entropy1;
+		};
 		
-		return findThresholdWithHighestMeasure(histogramData, pixelCount, a);
+		return findThresholdWithHighestMeasure(histogramData, pixelCount, measureCalculator);
 	}
 	
 	public static int findThresholdForLuWu(int[] histogramData, int pixelCount) {
-		BiFunction<int[], Integer, ToDoubleBiFunction<Integer, Integer>> a =
-				(int[] _histogramData, Integer _pixelCount) -> {
-					
-					return (Integer lessThenThresholdCount, Integer currThreshold) -> {
-						double entropy0 = entropy(
-								_histogramData,
-								0,
-								currThreshold,
-								lessThenThresholdCount);
+		MeasureCalculator measureCalculator = (
+				_histogramData,
+				_pixelCount,
+				lessThenThresholdCount,
+				currThreshold) -> {
+			
+			double entropy0 = entropy(
+					_histogramData,
+					0,
+					currThreshold,
+					lessThenThresholdCount);
 						
-						double entropy1 = entropy(
-								_histogramData,
-								currThreshold,
-								_histogramData.length,
-								_pixelCount - lessThenThresholdCount);
-						
-						
-						return -(entropy1 - entropy0);
-					};
-				};
+			double entropy1 = entropy(
+					_histogramData,
+					currThreshold,
+					_histogramData.length,
+					_pixelCount - lessThenThresholdCount);
+			
+			
+			return -(entropy1 - entropy0);
+		};
 		
-		return findThresholdWithHighestMeasure(histogramData, pixelCount, a);
+		return findThresholdWithHighestMeasure(histogramData, pixelCount, measureCalculator);
 	}
 	
 	
 	public static int findThresholdWithHighestMeasure(
 			int[] histogramData,
 			int pixelCount,
-			BiFunction<int[], Integer, ToDoubleBiFunction<Integer, Integer>> measureCalculatorSupplier) {
+			MeasureCalculator measureCalculator) {
 		
 		double valueOfMax = 0;
 		int indexOfMax = 0;
 		
 		int lessThenThresholdCount = 0;
 		
-		var measureCalculator =
-				measureCalculatorSupplier.apply(histogramData, pixelCount);
 		
 		for (int currThreshold = 1; currThreshold < histogramData.length; currThreshold++) {
 			lessThenThresholdCount += histogramData[currThreshold];
 			
-			double currMeasureValue = measureCalculator.applyAsDouble(lessThenThresholdCount, currThreshold);
+			double currMeasureValue = measureCalculator.calculate(histogramData, pixelCount, lessThenThresholdCount, currThreshold);
 			
 			if (valueOfMax < currMeasureValue) {
 				valueOfMax = currMeasureValue;
@@ -108,6 +106,22 @@ public class ThresholdingUtil {
 		
 		return indexOfMax;
 	}
+	
+	@FunctionalInterface
+	public interface MeasureCalculator {
+		/**
+		 * Calculates measure for a specified histogram and threshold
+		 *
+		 * @param histogramData amount of pixels corresponding to specified brightness level
+		 * @param allPixels
+		 * @param belowThreshold
+		 * @param currThreshold
+		 * @return calculated measure value
+		 */
+		double calculate(int[] histogramData, int allPixels, int belowThreshold, int currThreshold);
+	}
+	
+	
 	
 	private static double standardDev(int[] histogramData, int start, int end, int count) {
 		double mean = mean(histogramData, start, end, count);
